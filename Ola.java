ed7235f98b77499ac46b792ba91f2ac045e23154
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-02-16 17:49:40 +0000

    teste calc 16-02

--- Ola.java
+++ Ola.java
@@ -1,5 +1,10 @@
 public class Ola {
+    public static int soma (int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args){
         System.out.println("Olá Mundo!!!");
+        System.out.println(soma(17, 5)); 
+    
     }
 }    
